520f11bd704ae133518c49fb211cc2b99f0fb9d9
JoshVB <josh.vanbakkum@gmail.com>
2019-07-29 10:29:21 +1000

    dge script added

---
+++ dge/run_deseq2.R
@@ -0,0 +1,68 @@
+library("tidyverse")
+library("reshape2")
+library("DESeq2")
+library("gplots")
+
+# read counts
+tmp<-read.table("3col.tsv",header=F)
+x<-as.matrix(acast(tmp, V2~V1, value.var="V3", fun.aggregate = sum))
+x<-as.data.frame(x)
+accession<-sapply((strsplit(rownames(x),"\\|")),"[[",2)
+symbol<-sapply((strsplit(rownames(x),"\\|")),"[[",6)
+x$geneid<-paste(accession,symbol)
+
+xx<-aggregate(. ~ geneid,x,sum)
+rownames(xx)<-xx$geneid
+xx$geneid=NULL
+xx<-round(xx)
+
+pdf("MDSplot.pdf")
+plot(cmdscale(dist(t(xx))), xlab="Coordinate 1", ylab="Coordinate 2", type = "n") ; text(cmdscale(dist(t(xx))), labels=colnames(xx)) 
+dev.off()
+
+# curate the samplesheet
+samplesheet<-as.data.frame(colnames(xx))
+samplesheet$hcy<-c(0,0,0,1,1,1)
+rownames(samplesheet)<-samplesheet[,1]
+samplesheet[,1]=NULL
+
+####################################################
+# ctrl vs hcy
+####################################################
+y<-xx[which(rowSums(xx)/ncol(xx)>=(20)),]
+
+dds <- DESeqDataSetFromMatrix(countData = y , colData = samplesheet, design = ~ hcy )
+res <- DESeq(dds)
+z<- results(res)
+vsd <- vst(dds, blind=FALSE)
+zz<-cbind(as.data.frame(z),assay(vsd))
+dge<-as.data.frame(zz[order(zz$pvalue),])
+write.table(dge,file="hepg2_hcy_deseq.tsv",quote=F,sep="\t")
+rnk<-as.data.frame( sign(dge$log2FoldChange) * (-log(dge$pvalue + 1E-307) ))
+rownames(rnk)<-row.names(dge)
+colnames(rnk)="Score"
+write.table(rnk,file="hepg2_hcy_deseq.rnk",sep='\t',quote=F)
+
+#some plots
+pdf("hepg2_hcy_plots.pdf")
+sig<-subset(dge,padj<0.05)
+SIG=nrow(sig)
+DN=nrow(subset(sig,log2FoldChange<0))
+UP=nrow(subset(sig,log2FoldChange>0))
+HEADER=paste("Ctrl vs 200 uM Hcy:", SIG , "DGEs,", UP ,"upregulated,", DN, "downregulated")
+plot(log2(dge$baseMean),dge$log2FoldChange,cex=0.6, xlab="log2 base mean", ylim=c(-3,3),ylab="log2 fold change" ,pch=19,col="#838383")
+points(log2(sig$baseMean),sig$log2FoldChange,cex=0.6,pch=19,col="red")
+mtext(HEADER)
+top<-head(sig,20)
+#text(log2(top$baseMean)+1, top$log2FoldChange, labels = rownames(top),cex=0.7)
+#volcano plot
+plot(dge$log2FoldChange, -log2(dge$pvalue) ,cex=0.6, xlim=c(-3,3),xlab="log2 fold change", ylab="-log2 p-value" ,pch=19,col="#838383")
+points(sig$log2FoldChange, -log2(sig$pvalue),cex=0.6,pch=19,col="red")
+#text(top$log2FoldChange+0.5, -log2(top$pvalue), labels = rownames(top),cex=0.7)
+mtext(HEADER)
+# top N gene heatmap
+colfunc <- colorRampPalette(c("blue", "white", "red"))
+heatmap.2(  as.matrix(dge[1:50,c(7:ncol(dge))]), col=colfunc(25),scale="row",
+ trace="none",margins = c(6,15), cexRow=.6, cexCol=.8,  main="Top 100 genes")
+dev.off()
+
