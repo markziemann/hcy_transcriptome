dba6fa6b8daf50423759b9812f5bf695def9d8f9
JoshVB <josh.vanbakkum@gmail.com>
2019-08-18 16:26:56 +1000

    Added Write table

--- dge/run_deseq2.R
+++ dge/run_deseq2.R
@@ -1,3 +1,4 @@
+
 library("tidyverse")
 library("reshape2")
 library("DESeq2")
@@ -18,7 +19,8 @@ xx$geneid=NULL
 xx<-round(xx)
 
 pdf("MDSplot.pdf")
-plot(cmdscale(dist(t(xx))), xlab="Coordinate 1", ylab="Coordinate 2", type = "n") ; text(cmdscale(dist(t(xx))), labels=colnames(xx)) 
+plot(cmdscale(dist(t(xx))), xlab="Coordinate 1", ylab="Coordinate 2", type = "n") 
+text(cmdscale(dist(t(xx))), labels=colnames(xx)) 
 dev.off()
 
 # curate the samplesheet
@@ -51,7 +53,8 @@ SIG=nrow(sig)
 DN=nrow(subset(sig,log2FoldChange<0))
 UP=nrow(subset(sig,log2FoldChange>0))
 HEADER=paste("Ctrl vs 200 uM Hcy:", SIG , "DGEs,", UP ,"upregulated,", DN, "downregulated")
-plot(log2(dge$baseMean),dge$log2FoldChange,cex=0.6, xlab="log2 base mean", ylim=c(-3,3),ylab="log2 fold change" ,pch=19,col="#838383")
+plot(log2(dge$baseMean),dge$log2FoldChange,cex=0.6, xlab="log2 base mean", 
+ ylim=c(-3,3),ylab="log2 fold change" ,pch=19,col="#838383")
 points(log2(sig$baseMean),sig$log2FoldChange,cex=0.6,pch=19,col="red")
 mtext(HEADER)
 top<-head(sig,20)
@@ -83,12 +86,13 @@ res2
 
 stat<- deframe(res2)
 
-download.file("https://reactome.org/download/current/ReactomePathways.gmt.zip",destfile="ReactomePathways.gmt.zip")
+download.file("https://reactome.org/download/current/ReactomePathways.gmt.zip",
+ destfile="ReactomePathways.gmt.zip")
 unzip("ReactomePathways.gmt.zip")
 
 gsets<-gmtPathways("ReactomePathways.gmt")
 
-fgseaRes <- fgsea(pathways=gsets, stats=stat, nperm=10000)
+fgseaRes <- fgsea(pathways=gsets, stats=stat, minSize=10, nperm=100000)
 
 fgseaRes<-fgseaRes[order(fgseaRes$pval),]
 
@@ -99,5 +103,31 @@ head(subset(fgseaRes,ES>0),10)
 # peek at downregulated pathways
 head(subset(fgseaRes,ES<0),10)
 
+pdf("pathway_charts.pdf")
+
+psig<-subset(fgseaRes,pval<=0.05)
+plot(fgseaRes$ES,-log10(fgseaRes$pval),pch=19,cex=0.8,xlab="ES",ylab="-log10(p-value)")
+points(sig$ES,-log10(sig$pval),pch=19,cex=0.8,xlab="ES",ylab="-log10(p-value)",col="red")
+TOTAL=nrow(fgseaRes)
+SIG=nrow(sig)
+UP=length(which(sig$ES>0))
+DN=length(which(sig$ES<0))
+HEADER=paste(TOTAL,"gene sets examined,",SIG,"FDR<0.05,",UP,"up-regulated,",DN,"down-regulated")
+mtext(HEADER)
+
+# barchart top effect size FDR<0.05
+par(mfrow = c(2, 1)) 
+sig_up<-subset(sig,ES>0)
+sig_dn<-subset(sig,ES<0)
+sig_up<-head(sig_up[order(-sig_up$ES),],20)
+sig_dn<-head(sig_dn[order(-sig_dn$ES),],20)
+
+par( mar=c(5,28,4,2)) ; barplot(rev(sig_up$ES),horiz=T,names.arg=rev(sig_up$pathway),las=2, cex.names=0.6 , cex.axis=0.6, xlab="ES",main="up-regulated sets",cex.main=0.6)
+par( mar=c(5,28,4,2)) ; barplot(rev(sig_dn$ES),horiz=T,names.arg=rev(sig_dn$pathway),las=2, cex.names=0.6, cex.axis=0.6, xlab="ES",main="down-regulated sets",cex.main=0.6)
+dev.off()
+# barchart top significant
 
 
+# Pathway Table
+psig$leadingEdge <- vapply(psig$leadingEdge, paste, collapse = ", ", character(1L))
+write.table(psig, file = "Pathway_tables.csv", sep = ",")
